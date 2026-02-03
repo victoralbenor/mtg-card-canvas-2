94c64e9abd432c213e61fee7b640c10e0a3ea68b
Victor Albenor <victoralbenor@gmail.com>
2026-02-03 14:39:18 -0300

    change path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mtg-card-canvas/',
+  server: {
+    port: 5174
+  }
 })
