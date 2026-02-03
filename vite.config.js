e0af004e9ee2b0d20e061b0bb487bc0dca0aedce
Victor Albenor <victoralbenor@gmail.com>
2026-02-03 14:53:13 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mtg-card-canvas/',
+  base: '/mtg-card-canvas-2/',
   server: {
     port: 5174
   }
